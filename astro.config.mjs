888beb2f6bfdf5ef5aa1f9d778dd3ab143ccc0b5
Muhammad <muhammadhabib00001@gmail.com>
2026-09-21 18:08:49 +0500

    Optimize king bed set slug to how-to-style-luxury-king-bed-set for search engine performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.atriumlivings.com',
   redirects: {
-    '/architectural-master-planning-king-bed-set': '/how-to-style-a-luxury-king-bed-set-master-bedroom-guide',
-    '/how-to-style-a-luxury-king-bed-set': '/how-to-style-a-luxury-king-bed-set-master-bedroom-guide'
+    '/architectural-master-planning-king-bed-set': '/how-to-style-luxury-king-bed-set',
+    '/how-to-style-a-luxury-king-bed-set': '/how-to-style-luxury-king-bed-set',
+    '/how-to-style-a-luxury-king-bed-set-master-bedroom-guide': '/how-to-style-luxury-king-bed-set',
+    '/luxury-king-bed-set-guide': '/how-to-style-luxury-king-bed-set'
   },
   integrations: [],
   server: {
